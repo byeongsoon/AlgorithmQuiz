deea720b8825f6c152c1fa2f51fc99004473085a
Byeongsoon Jang <byeongsoon94@gmail.com>
2022-04-06 22:32:31 +0900

    가장 큰 수 구하기 수정

--- programmers/src/Level2/BiggestNumber.java
+++ programmers/src/Level2/BiggestNumber.java
@@ -1,18 +1,15 @@
 package Level2;
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class BiggestNumber {
 
     public String solution(int[] numbers) {
-        String answer = "";
         String[] numberString = new String[numbers.length];
 
         for (int i = 0; i < numbers.length; i++) {
-//            numberString[i] = String.valueOf(numbers[i]);
             numberString[i] = Integer.toString(numbers[i]);
         }
 
@@ -25,22 +22,18 @@ public class BiggestNumber {
 
         if (numberString[0].equals("0")) return "0"; // 0으로만 이루어진 경우
 
-        answer = String.join("",numberString);
-
-        return answer;
+        return String.join("",numberString);
     }
 
     public static void main(String[] args) {
         BiggestNumber test = new BiggestNumber();
 
 //        int[] input = {6,10,2};
-//        int[] input = {3, 30, 34, 5, 9, 40};
-        int[] input = {0,0,0};
+        int[] input = {3, 30, 34, 5, 9, 40};
+//        int[] input = {0,0,0};
 
         String result = test.solution(input);
         System.out.println(result);
-
-
     }
 
 }
