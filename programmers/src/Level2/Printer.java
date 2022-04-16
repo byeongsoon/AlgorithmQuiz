d702ccbe774e073ffed2c00a0211e61e41076ac7
Byeongsoon Jang <byeongsoon94@gmail.com>
2022-04-16 23:07:54 +0900

    프린터 문제

---
+++ programmers/src/Level2/Printer.java
@@ -0,0 +1,16 @@
+package Level2;
+
+public class Printer {
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] priorities = {2,1,3,2};
+        // int[] priorities = {1,1,9,1,1,1};
+    }
+
+}
