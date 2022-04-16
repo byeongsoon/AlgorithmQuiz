412d48b0150ea3b037574daf652e9a490976c9ea
Byeongsoon Jang <byeongsoon94@gmail.com>
2022-04-17 01:49:17 +0900

    프린터 문제 히든케이스 적용을 위한 변경준비

--- programmers/src/Level2/Printer.java
+++ programmers/src/Level2/Printer.java
@@ -7,8 +7,7 @@ public class Printer {
 
     public static int solution(int[] priorities, int location) {
         int answer = location + 1;
-        Integer[] intArray = new Integer[priorities.length];
-
+        int count = 0;
         ArrayList<Integer> queue = new ArrayList<>(priorities.length);
 
         // 인쇄 대기목록에 문서들 집어넣기
@@ -25,11 +24,18 @@ public class Printer {
                 queue.remove(0);
                 queue.add(tmp);
 
-                // 첫 인자가 인쇄되지않고 마지막에 추가된다면 순서 변경
-                answer = (answer == 1)? answer + priorities.length-1 : answer-1;
-            } else { // 3. 그렇지 않으면 tmp를 인쇄 -> remove()
+//                answer = (answer == 1)? queue.size() : answer-1;
+
+                // 지금의 문제는 처음부터 출력되거나 중간중간 출력되는 경우를 고민하지 않았다..
+                // 제공되는 2개의 테스트는 모두 정렬이 이루어진 뒤 쭉 출력이었고
+                // 중간중간 출력도하면서 이동도하는 경우를 생각하며 답을 바꿔야한다.
+
+
+            } else { // 3. 그렇지 않으면 tmp 를 인쇄 -> remove()
                 queue.remove(0);
+                count++;
             }
+
         }
 
         return answer;
@@ -38,9 +44,11 @@ public class Printer {
     public static void main(String[] args) {
         int[] priorities = {2,1,3,2}; // 중요도
         int[] priorities2 = {1,1,9,1,1,1};
+        int[] priorities3 = {2,1,2,1,2,1,2,1,2};
 
-        System.out.println(solution(priorities,2));
-        System.out.println(solution(priorities2,0));
+        System.out.println(solution(priorities,2)); // 1
+        System.out.println(solution(priorities2,0)); // 5
+        System.out.println(solution(priorities3,1)); // 6
     }
 
 }
