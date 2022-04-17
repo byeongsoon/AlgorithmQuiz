04f9a7fc61d705338c1849250ed3cda5176e0ccc
Byeongsoon Jang <byeongsoon94@gmail.com>
2022-04-17 11:26:39 +0900

    프린터 문제 수정

--- programmers/src/Level2/Printer.java
+++ programmers/src/Level2/Printer.java
@@ -6,8 +6,8 @@ import java.util.Collections;
 public class Printer {
 
     public static int solution(int[] priorities, int location) {
-        int answer = location + 1;
-        int count = 0;
+        int answer = 0;
+        location += 1;
         ArrayList<Integer> queue = new ArrayList<>(priorities.length);
 
         // 인쇄 대기목록에 문서들 집어넣기
@@ -23,17 +23,17 @@ public class Printer {
             if (tmp < max) { // 2. tmp 보다 중요도가 높은 문서가 존재한다면 대기목록 제일 마지막에 추가
                 queue.remove(0);
                 queue.add(tmp);
+                location--;
 
-//                answer = (answer == 1)? queue.size() : answer-1;
-
-                // 지금의 문제는 처음부터 출력되거나 중간중간 출력되는 경우를 고민하지 않았다..
-                // 제공되는 2개의 테스트는 모두 정렬이 이루어진 뒤 쭉 출력이었고
-                // 중간중간 출력도하면서 이동도하는 경우를 생각하며 답을 바꿔야한다.
-
-
+                if (location < 1)
+                    location = queue.size();
             } else { // 3. 그렇지 않으면 tmp 를 인쇄 -> remove()
                 queue.remove(0);
-                count++;
+                location--;
+                answer++;
+
+                if (location < 1)
+                    break;
             }
 
         }
