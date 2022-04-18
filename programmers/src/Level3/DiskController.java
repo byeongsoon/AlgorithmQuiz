a9a3caf175b44dac47bf6ed422f9cdc11a7aa2c9
Byeongsoon Jang <byeongsoon94@gmail.com>
2022-04-18 22:51:14 +0900

    디스크 컨트롤러 1차 풀이 - 주어진 테스트만 통과

--- programmers/src/Level3/DiskController.java
+++ programmers/src/Level3/DiskController.java
@@ -1,2 +1,41 @@
-package Level3;public class DiskController {
+package Level3;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+public class DiskController {
+
+    public static int solution(int[][] jobs) {
+        int answer = 0;
+        int totalTime = 0;
+
+        PriorityQueue<String> jobQueue = new PriorityQueue<>();
+
+        for (int[] arr: jobs)
+            jobQueue.add(arr[1]+","+arr[0]);
+
+        while (jobQueue.size() > 0) {
+
+            String[] arr = jobQueue.poll().split(",");
+            int job = Integer.parseInt(arr[0]);
+            int time = Integer.parseInt(arr[1]);
+
+            if (time > totalTime)
+                totalTime = time - totalTime;
+
+            totalTime += job;
+            answer += (totalTime-time);
+        }
+
+
+        return answer/jobs.length;
+    }
+
+    public static void main(String[] args) {
+        int[][] jobs = {{0,3}, {1,9}, {2,6}};
+        System.out.println(solution(jobs));
+    }
+
 }
