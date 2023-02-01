825ea9106e57bee9d2f682628163c9c92f334fb3
byeongsoon <byeongsoon94@gmail.com>
2023-02-01 19:12:12 +0900

    1주차 - 2월 23일 할당 알고리즘 풀이

---
+++ BES_3th/src/main/java/week1/day_0223/MultiplicationOfTwoNumbers.java
@@ -0,0 +1,9 @@
+package week1.day_0223;
+
+public class MultiplicationOfTwoNumbers {
+
+    public int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+
+}
---
+++ BES_3th/src/main/java/week1/day_0223/PrintAge.java
@@ -0,0 +1,10 @@
+package week1.day_0223;
+
+public class PrintAge {
+
+    public int solution(int age) {
+        int year = 2022;
+        return year - age + 1;
+    }
+
+}
