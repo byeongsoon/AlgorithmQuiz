256f12d64107e3b935833cb75d2cfe407ebd8421
byeongsoon <byeongsoon94@gmail.com>
2023-02-01 19:04:49 +0900

    1주차 - 2월 22일 할당 알고리즘 풀이

---
+++ BES_3th/src/main/java/week1/day_0222/CompareNumbers.java
@@ -0,0 +1,12 @@
+package week1.day_0222;
+
+public class CompareNumbers {
+
+    public int solution(int num1, int num2) {
+        if (num1 == num2) {
+            return 1;
+        }
+        return -1;
+    }
+
+}
---
+++ BES_3th/src/main/java/week1/day_0222/SubtractionOfTwoNumbers.java
@@ -0,0 +1,9 @@
+package week1.day_0222;
+
+public class SubtractionOfTwoNumbers {
+
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+
+}
