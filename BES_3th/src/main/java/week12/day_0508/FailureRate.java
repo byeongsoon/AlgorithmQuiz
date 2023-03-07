ca02c2cdd0d4f3096cc8925d0cab1a192139dcf0
byeongsoon <byeongsoon94@gmail.com>
2023-03-07 21:14:29 +0900

    12주차 - 5월 8일 : 실패율(2019 카카오)

---
+++ BES_3th/src/main/java/week12/day_0508/FailureRate.java
@@ -0,0 +1,57 @@
+package week12.day_0508;
+
+import java.util.*;
+
+public class FailureRate {
+
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int person = stages.length;
+        Map<Integer, Double> result = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            int userStay = stageStayUserCount(stages, i);
+
+            if (userStay == 0) {
+                result.put(i, 0.0);
+                continue;
+            }
+
+            double failureRate = (double) userStay / person;
+            result.put(i, failureRate);
+
+            person -= userStay;
+        }
+
+        List<Integer> keySet = new ArrayList<>(result.keySet());
+        keySet.sort(((o1, o2) -> result.get(o2).compareTo(result.get(o1))));
+
+        int i = 0;
+        for (double key: keySet) {
+            answer[i++] = (int) key;
+        }
+
+        return answer;
+    }
+
+    private int stageStayUserCount(int[] stages, int stage) {
+        int result = 0;
+
+        for (int n: stages) {
+            if (n == stage) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        FailureRate test = new FailureRate();
+        int N = 4;
+        int[] stages = {4,4,4,4,4};
+        test.solution(N, stages);
+    }
+
+}
