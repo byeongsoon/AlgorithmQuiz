e743ba6c18c9c2c836bc700aa05ef0964f9928e8
byeongsoon <byeongsoon94@gmail.com>
2023-03-29 15:15:09 +0900

    13주차 - 5월 17일 : 신고 결과 받기(다른 사람 풀이 추가)

--- BES_3th/src/main/java/week13/day_0517/DeclarationResultReport.java
+++ BES_3th/src/main/java/week13/day_0517/DeclarationResultReport.java
@@ -1,13 +1,14 @@
 package week13.day_0517;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DeclarationResultReport {
 
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         Set<String> reportSet = new HashSet<>(Arrays.asList(report));
-        Map<String, Integer> declaration = new HashMap<>(); // 유저 별로 신고당한 횟수
+        Map<String, Integer> count = new HashMap<>(); // 유저 별로 신고당한 횟수
         Map<String, List<String>> reportUser = new HashMap<>(); // 유저 ID와 유저가 신고한 ID
 
         for (String s : reportSet) {
@@ -21,13 +22,13 @@ public class DeclarationResultReport {
             reportUser.put(user, list);
 
             // 신고받은 횟수 추가
-            declaration.put(reportedUser, declaration.getOrDefault(reportedUser,0) + 1);
+            count.put(reportedUser, count.getOrDefault(reportedUser,0) + 1);
         }
 
         // 이용 정지 대상자 색출
         List<String> stopUse = new ArrayList<>();
-        for (String s : declaration.keySet()) {
-            if (declaration.get(s) >= k) {
+        for (String s : count.keySet()) {
+            if (count.get(s) >= k) {
                 stopUse.add(s);
             }
         }
@@ -45,13 +46,19 @@ public class DeclarationResultReport {
         return answer;
     }
 
-    public static void main(String[] args) {
-//        String[] id_list = {"con", "ryan"};
-//        String[] report = {"ryan con","ryan con","ryan con","ryan con"};
-        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
-        DeclarationResultReport test = new DeclarationResultReport();
-        test.solution(id_list, report, 2);
+    public int[] otherSolution(String[] id_list, String[] report, int k) {
+        List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
+        HashMap<String, Integer> count = new HashMap<>();
+        for (String s : list) {
+            String target = s.split(" ")[1];
+            count.put(target, count.getOrDefault(target, 0) + 1);
+        }
+
+        return Arrays.stream(id_list).map(_user -> {
+            final String user = _user;
+            List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
+            return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
+        }).mapToInt(Long::intValue).toArray();
     }
 
 }
