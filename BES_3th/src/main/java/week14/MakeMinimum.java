d046ca7ed3da36b025708166d9e476029c7164aa
byeongsoon <byeongsoon94@gmail.com>
2023-03-30 15:05:40 +0900

    14주차 - 최솟값 만들기

---
+++ BES_3th/src/main/java/week14/MakeMinimum.java
@@ -0,0 +1,22 @@
+package week14;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MakeMinimum {
+
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Integer[] tempB = Arrays.stream(B).boxed().toArray(Integer[]::new);
+
+        Arrays.sort(A);
+        Arrays.sort(tempB, Collections.reverseOrder());
+
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * tempB[i];
+        }
+
+        return (Integer) answer;
+    }
+
+}
