4c591f2d4f071d273b34a406749f0d0edbde9037
byeongsoon <byeongsoon94@gmail.com>
2023-03-30 09:17:27 +0900

    14주차 - 최댓값과 최솟값

--- BES_3th/src/main/java/etc/MaxAndMinValue.java
+++ BES_3th/src/main/java/etc/MaxAndMinValue.java
@@ -1,2 +1,20 @@
-package etc;public class MaxAndMinValue {
+package etc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MaxAndMinValue {
+
+    public String solution(String s) {
+        List<Integer> numbers = Arrays.stream(s.split(" "))
+            .map(Integer::parseInt)
+            .sorted()
+            .collect(Collectors.toList());
+
+        return numbers.get(0) + " " + numbers.get(numbers.size()-1);
+    }
+
 }
