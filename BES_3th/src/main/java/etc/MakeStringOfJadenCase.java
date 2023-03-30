4eaa472b2f9134b69bff740b3b8a4f628e238724
byeongsoon <byeongsoon94@gmail.com>
2023-03-30 14:41:09 +0900

    14주차 - JadenCase 문자열 만들기

--- BES_3th/src/main/java/etc/MakeStringOfJadenCase.java
+++ BES_3th/src/main/java/etc/MakeStringOfJadenCase.java
@@ -1,2 +1,47 @@
-package etc;public class MakeStringOfJadenCase {
+package etc;
+
+import java.util.Objects;
+
+public class MakeStringOfJadenCase {
+    public String solution(String s) {
+        s = s.toLowerCase();
+        String[] inputArray = s.split("");
+        String[] answer = new String[inputArray.length];
+
+        String before = "";
+        for (int i = 0; i < inputArray.length; i++) {
+            if ("".equals(before) || (" ".equals(before) && !isInteger(inputArray[i]))) {
+                before = inputArray[i];
+                answer[i] = inputArray[i].toUpperCase();
+                continue;
+            }
+            before = inputArray[i];
+            answer[i] = inputArray[i];
+        }
+
+        return String.join("", answer);
+    }
+
+    private boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public String otherSolution(String s) {
+        String answer = "";
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
+        }
+
+        return answer;
+    }
+
 }
