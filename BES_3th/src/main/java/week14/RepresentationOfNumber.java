1ca811c98ad6458691d3cfd0b05acab75d609238
byeongsoon <byeongsoon94@gmail.com>
2023-03-31 13:18:55 +0900

    14주차 - 숫자의 표현

---
+++ BES_3th/src/main/java/week14/RepresentationOfNumber.java
@@ -0,0 +1,29 @@
+package week14;
+
+public class RepresentationOfNumber {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == n) {
+                answer++;
+                break;
+            }
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+
+}
