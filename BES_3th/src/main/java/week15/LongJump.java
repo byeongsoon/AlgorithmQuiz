8ae6c416f75a88da45854b6cbfd3518f8cc59812
byeongsoon <byeongsoon94@gmail.com>
2023-04-06 11:29:54 +0900

    15주차 멀리뛰기

---
+++ BES_3th/src/main/java/week15/LongJump.java
@@ -0,0 +1,24 @@
+package week15;
+
+public class LongJump {
+
+    public long solution(int n) {
+        long[] fibonacci = new long[n + 1];
+
+        if(n == 1) {
+            return 1;
+        } else if(n == 2) {
+            return 2;
+        }
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        fibonacci[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            fibonacci[i] = (fibonacci[i-2] + fibonacci[i-1]) % 1234567;
+        }
+
+        return fibonacci[n];
+    }
+}
