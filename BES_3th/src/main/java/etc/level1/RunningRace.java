072722092c427977b2ca15310c3336da5911e858
byeongsoon <byeongsoon94@gmail.com>
2023-04-08 23:04:22 +0900

    feat : Level1 - 달리기 경주(swap 부분 Map으로 직접 구현

--- BES_3th/src/main/java/etc/level1/RunningRace.java
+++ BES_3th/src/main/java/etc/level1/RunningRace.java
@@ -1,25 +1,32 @@
 package etc.level1;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class RunningRace {
 
     public String[] solution(String[] players, String[] callings) {
         String[] answer = new String[players.length];
-        List<String> list = new ArrayList<>();
-        for (String player : players) {
-            list.add(player);
+        Map<Integer, String> indexMap = new HashMap<>();
+        Map<String, Integer> playerMap = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++) {
+            indexMap.put(i,players[i]);
+            playerMap.put(players[i],i);
         }
 
         for (String call : callings) {
-            int index = list.indexOf(call);
-            Collections.swap(list, index, index - 1);
+            int index = playerMap.get(call);
+            String temp = indexMap.get(index - 1);
+
+            playerMap.put(call, index - 1);
+            playerMap.put(temp, index);
+
+            indexMap.put(index - 1, call);
+            indexMap.put(index, temp);
         }
 
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
+            answer[i] = indexMap.get(i);
         }
 
         return answer;
