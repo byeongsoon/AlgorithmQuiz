0a11318c4c5d2dd72176ef4b037c26a4c58ca4ef
byeongsoon <byeongsoon94@gmail.com>
2023-04-27 10:27:17 +0900

    Level2 - 기능구현(큐 이용 풀이법 추가)

--- BES_3th/src/main/java/etc/level2/FunctionDevelopment.java
+++ BES_3th/src/main/java/etc/level2/FunctionDevelopment.java
@@ -1,7 +1,11 @@
 package etc.level2;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FunctionDevelopment {
 
@@ -43,6 +47,29 @@ public class FunctionDevelopment {
         }
 
         return count;
+    }
+
+    public int[] otherSolution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = IntStream
+            .range(0, progresses.length)
+            .mapToObj(i -> (int) Math.ceil((100.0 - progresses[i]) / speeds[i]))
+            .collect(Collectors.toCollection(LinkedList::new));
+
+        List<Integer> answer = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            int remains = queue.poll();
+            int count = 1;
+
+            while (!queue.isEmpty() && remains >= queue.peek()) {
+                queue.poll();
+                count++;
+            }
+
+            answer.add(count);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 
 }
