681ee5c618ce3062f4b158b003e52942a7dbebae
byeongsoon <byeongsoon94@gmail.com>
2023-10-12 17:04:48 +0900

    Level 1 - 이름이 있는 동물 아이디

--- BES_3th/src/main/java/etc/sql/IsNull.sql
+++ BES_3th/src/main/java/etc/sql/IsNull.sql
@@ -1,5 +1,9 @@
-/* Level 1 이름이 있는 동물의 아이디 */
+/* Level 1 - 이름이 없는 동물의 아이디 */
 SELECT a.ANIMAL_ID
 FROM ANIMAL_INS as a
 WHERE a.NAME IS NULL;
 
+/* Level 1 - 이름이 있는 동물의 아이디 */
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL;
