dba0efdcc747a25c8df45c12faa2be5667de3781
byeongsoon <byeongsoon94@gmail.com>
2023-10-24 19:01:38 +0900

    레벨별 문제 위치 내림차순으로 수정

--- BES_3th/src/main/java/etc/sql/SumMaxMin.sql
+++ BES_3th/src/main/java/etc/sql/SumMaxMin.sql
@@ -1,15 +1,3 @@
-/* Level 2 - 중복 제거하기 */
-SELECT DISTINCT COUNT(DISTINCT a.NAME) AS 'count'
-FROM ANIMAL_INS a;
-
-/* Level 1 - 가장 비싼 상품 구하기 */
-SELECT MAX(PRICE) AS MAX_PRICE
-FROM PRODUCT;
-
-/* Level 1 - 최댓값 구하기 */
-SELECT MAX(DATETIME) AS '시간'
-FROM ANIMAL_INS;
-
 /* Level 2 - 최솟값 구하기 */
 SELECT MIN(DATETIME) AS '시간'
 FROM ANIMAL_INS;
@@ -29,3 +17,15 @@ WHERE PRICE = (
     SELECT MAX(PRICE)
     FROM FOOD_PRODUCT
 );
+
+/* Level 2 - 중복 제거하기 */
+SELECT DISTINCT COUNT(DISTINCT a.NAME) AS 'count'
+FROM ANIMAL_INS a;
+
+/* Level 1 - 가장 비싼 상품 구하기 */
+SELECT MAX(PRICE) AS MAX_PRICE
+FROM PRODUCT;
+
+/* Level 1 - 최댓값 구하기 */
+SELECT MAX(DATETIME) AS '시간'
+FROM ANIMAL_INS;
